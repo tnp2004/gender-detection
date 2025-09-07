7e2ccd7acbf23b612f69c864d73cc61bda64acba
tnp2004 <iceandiceisice2004@gmail.com>
2025-09-07 13:54:01 +0700

    feat: add API routes for cameras and locations

--- web/app/api/cameras/route.ts
+++ web/app/api/cameras/route.ts
@@ -1,14 +1,14 @@
 import { sql, Table } from "@/database/db"
 import { NextResponse } from "next/server"
 
-const table = Table.GenderLog
+const table = Table.Camera
 
 export const GET = async () => {
     try {
         const result = await sql`SELECT * FROM ${sql(table)}`
         return NextResponse.json(result)
     } catch (err) {
-        console.error("[ERROR] Gender logs:", (err instanceof Error ? err.message : String(err)))
-        return NextResponse.json({ message: "cannot get gender logs"})
+        console.error("[ERROR] Cameras:", (err instanceof Error ? err.message : String(err)))
+        return NextResponse.json({ message: "cannot get cameras"})
     }
 }
