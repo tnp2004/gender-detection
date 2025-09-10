93af7673cd7fe90ee844f589f71fca617bf5dea8
tnp2004 <iceandiceisice2004@gmail.com>
2025-09-10 13:49:54 +0700

    feat: add row counter api to cameras, genderLogs, locations

---
+++ web/app/api/cameras/count/route.ts
@@ -0,0 +1,15 @@
+import { sql, Table } from "@/database/db"
+import { NextResponse } from "next/server"
+
+const table = Table.Camera
+
+export const GET = async () => {
+    try {
+        let [result] = await sql`SELECT COUNT(*) FROM ${sql(table)}`
+        result.count = Number(result.count)
+        return NextResponse.json(result)
+    } catch (err) {
+        console.error("[ERROR] Cameras:", (err instanceof Error ? err.message : String(err)))
+        return NextResponse.json({ message: "cannot count cameras"})
+    }
+}
