baf826061d1ec6f7b5bb33fcdd3f8eb8f6b4f93d
tnp2004 <iceandiceisice2004@gmail.com>
2025-10-06 21:45:14 +0700

    fix: keep tracking when no people

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from db import createGenderLog, findLocationByCameraId, activeCamera, inactiveCa
 from file import getMetadata
 from utils.log import printGenderLog, printQuitLog
 
+
 class Gender(Enum):
     MAN = "Man"
     WOMAN = "Woman"
+
 
 NAME = "Gender detector"
 YOLO_MODEL = "yolo12m.pt"
@@ -33,6 +35,7 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise RuntimeError("Error: Cannot open video file.")
 
+
 def get_window_size(cap):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -40,6 +43,7 @@ def get_window_size(cap):
         min(width, MAX_WINDOW_WIDTH_SIZE),
         min(height, MAX_WINDOW_HEIGHT_SIZE)
     )
+
 
 w, h = get_window_size(cap)
 cv2.namedWindow(NAME, cv2.WINDOW_NORMAL)
@@ -52,17 +56,20 @@ cameraId = metadata["cameraId"]
 locationInfo = findLocationByCameraId(cameraId)[0]
 locationId = locationInfo.locationId
 
+
 def addGenderLog(gender: str):
     return createGenderLog({
         "locationId": locationId,
         "gender": gender
     })
 
+
 def drawTrackingBox(frame, trackId, pt1, pt2):
     cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 2)
     label = peopleData.get(trackId, "analyzing")
     cv2.putText(frame, label, (pt1[0], pt1[1] - 10),
                 cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 0), 2)
+
 
 def main():
     activeCamera(cameraId)
@@ -81,36 +88,31 @@ def main():
             conf=0.25
         )
 
-        if not results or not results[0].boxes:
-            continue
+        if results and results[0].boxes and results[0].boxes.is_track:
+            boxesData = results[0].boxes
+            boxes = boxesData.xyxy.cpu()
+            trackIds = boxesData.id.int().cpu().tolist()
+            fw = frame.shape[1]
 
-        boxesData = results[0].boxes
-        if not boxesData.is_track:
-            continue
+            for box, trackId in zip(boxes, trackIds):
+                x1, y1, x2, y2 = map(int, box)
 
-        boxes = boxesData.xyxy.cpu()
-        trackIds = boxesData.id.int().cpu().tolist()
-        fw = frame.shape[1]
+                if trackId not in peopleData and (x1 > detectMargin and x2 < (fw - detectMargin)):
+                    cropMargin = 10
+                    x1m, y1m = max(0, x1 - cropMargin), max(0, y1 - cropMargin)
+                    x2m, y2m = min(fw, x2 + cropMargin), min(frame.shape[0], y2 + cropMargin)
+                    cropped = frame[y1m:y2m, x1m:x2m]
 
-        for box, trackId in zip(boxes, trackIds):
-            x1, y1, x2, y2 = map(int, box)
+                    faces = app.get(cropped)
+                    if faces:
+                        face = faces[0]
+                        gender = genderLabels[face.gender].value
+                        peopleData[trackId] = gender
 
-            if trackId not in peopleData and (x1 > detectMargin and x2 < (fw - detectMargin)):
-                cropMargin = 10
-                x1m, y1m = max(0, x1 - cropMargin), max(0, y1 - cropMargin)
-                x2m, y2m = min(fw, x2 + cropMargin), min(frame.shape[0], y2 + cropMargin)
-                cropped = frame[y1m:y2m, x1m:x2m]
+                        genderLog = addGenderLog(gender)
+                        printGenderLog(gender, genderLog.detectedAt)
 
-                faces = app.get(cropped)
-                if faces:
-                    face = faces[0]
-                    gender = genderLabels[face.gender].value
-                    peopleData[trackId] = gender
-
-                    genderLog = addGenderLog(gender)
-                    printGenderLog(gender, genderLog.detectedAt)
-
-            drawTrackingBox(frame, trackId, (x1, y1), (x2, y2))
+                drawTrackingBox(frame, trackId, (x1, y1), (x2, y2))
 
         scaled = cv2.resize(frame, (w, h))
         cv2.imshow(NAME, scaled)
